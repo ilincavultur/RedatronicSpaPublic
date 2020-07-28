69f81ece05aff4ae199db190abf11ed9d4566b0b
ilincaav <ilinca_vultur17@yahoo.com>
2020-07-28 10:08:44 +0200

    new

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,11 +5,13 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Form\SearchProductType;
 use App\Repository\ProductRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use phpDocumentor\Reflection\Types\Integer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,6 +24,7 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\Form\FormTypeInterface;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 
 
@@ -55,9 +58,6 @@ class ProductController extends AbstractController
             $product->setWeekendPrice($data->getWeekendPrice());
             $product->setType($data->getType());
 
-
-
-
             $em->persist($product);
             $em->flush();
             return $this->redirectToRoute('app_product_list');
@@ -71,22 +71,34 @@ class ProductController extends AbstractController
 
     }
 
-
     /**
      * @Route("/list", name="app_product_list")
      *
-     * @param ProductRepository $productRepository
+     * @param Request $request
+     *
+     *
      * @return Response
      */
-    public function listAction(ProductRepository $productRepository)
+    public function listAction(Request $request)
     {
-        $products = $productRepository->findAll();
+        $productRepository = $this->getDoctrine()->getRepository(Product::class);
+        $qb = $productRepository->findProducts($request->get('search'));
 
-        return $this->render('Product/list.html.twig', [
-            'products'=>$products
-        ]);
+        $page = $request->get('page');
+        $pager = new Pagerfanta(new DoctrineORMAdapter($qb));
+        $pager->setCurrentPage($page?$page:1);
+        $pager->getNbResults();
+
+        return $this->render(
+            'Product/list.html.twig',
+            [
+                'pager' => $pager
+            ]
+        );
 
     }
+
+
 
     /**
      * @Route("/{id}/delete", name="app_product_delete")
@@ -110,42 +122,44 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/edit/{id}")
+     * @Route("/edit/{id}", name="app_product_edit")
+     * @param Request $request
      * @param $id
-     * @return RedirectResponse
+     * @return Response
      */
-    public function update($id)
+    public function update(Request $request, $id)
     {
         $entityManager = $this->getDoctrine()->getManager();
         $product = $entityManager->getRepository(Product::class)->find($id);
 
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+        $form = $this->createForm(\ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_product_list');
         }
 
-        $product->setName('New product name!');
-        $entityManager->flush();
-
-        return $this->redirectToRoute('app_product_list', [
-            'id' => $product->getId()
-        ]);
+        return $this->render(
+            'Product/editProduct.html.twig',
+            [
+                'user_form' => $form->createView()
+            ]
+        );
     }
 
 
 
-    /**
-     * @Route("/{id}/info", name="app_product_info")
-     *
-     * @param Product $product
-     * @return JsonResponse
-     */
-    public function productInfo(Product $product)
-    {
-        $response = ['id' => $product->getId(), 'name' => $product->getName(), 'code' => $product->getCode(), 'price' => $product->getPrice()];
-        return new JsonResponse($response);
-    }
+
+
+
+
+
+
+
+
 
 
 
