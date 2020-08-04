3dcd855e24685671f006ef5b7ea05cb3f8dc32d9
ilincaav <ilinca_vultur17@yahoo.com>
2020-08-04 12:29:19 +0200

    added: Packages(Create, List, Edit, Delete)

--- src/Form/PackageType.php
+++ src/Form/PackageType.php
@@ -4,7 +4,53 @@
 namespace App\Form;
 
 
-class PackageType
+use App\Entity\Package;
+use App\Entity\Zone;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class PackageType extends AbstractType
 {
+
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Name', TextType::class)
+            ->add('Price', IntegerType::class)
+            ->add('Zones', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Zone::class,
+
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'Name',
+
+                // used to render a select box, check boxes or radios
+                'multiple' => true,
+                'expanded' => true,
+            ]);
+        ;
+
+
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Package::class
+        ]);
+    }
 
 }
